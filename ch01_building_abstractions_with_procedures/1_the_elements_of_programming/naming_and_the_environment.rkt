1cd4bbc349190b14a08e2045b52a554698fc0e5e
Seonghyeon Kim <kim@seonghyeon.dev>
2020-02-29 17:39:08 +0900

    Add naming_and_the_environment.rkt
    - symbol of value
    - environment

---
+++ ch01_building_abstractions_with_procedures/1_the_elements_of_programming/naming_and_the_environment.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define size 2)
+
+size
+; 2
+
+(* 5 size)
+; 10
+
+(define pi 3.141592)
+
+(define radius 10)
+
+(* pi (* radius radius))
+; 314.1592
+
+(define circumference (* 2 pi radius))
+
+circumference
+;62.83184
